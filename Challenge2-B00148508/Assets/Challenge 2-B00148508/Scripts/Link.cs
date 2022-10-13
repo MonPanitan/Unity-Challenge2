d161bd77265cb46d7edcffaf93fdddb3d15e8ffa
Mon <98190812+MonPanitan@users.noreply.github.com>
2022-10-13 13:27:03 +0100

    Add Button
    Create a button and link of Code Repo to Github

---
+++ Challenge2-B00148508/Assets/Challenge 2-B00148508/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenCodeRepo()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://github.com/MonPanitan/Unity-Challenge2");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
